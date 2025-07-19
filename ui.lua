b9fad8abc514167e5fedcb0b8faeaf5cef0eed52
Mobile User <mobile@localhost>
2025-07-19 18:41:40 +1000

    save local changes at 2025-07-19 18:41:40

--- ui.lua
+++ ui.lua
@@ -2,7 +2,7 @@
 ProgressBar = {}
 ProgressBar.__index = ProgressBar
 
-function ProgressBar.new(x, y, w, h, mode)
+function ProgressBar.new(x, y, w, h, mode, colour)
 	local instance = setmetatable({}, ProgressBar)
 	if mode == nil then mode = "vertical" end
 	instance.mode = mode
@@ -10,16 +10,20 @@ function ProgressBar.new(x, y, w, h, mode)
 	instance.y = y
 	instance.w = w
 	instance.h = h
+	if not colour then colour = {1, 1, 1, 1} end
+	instance.colour = colour
 	return instance
 end
 
 function ProgressBar:draw(progress)
+	love.graphics.setColor(self.colour)
 	love.graphics.rectangle("line", self.x, self.y, self.w, self.h)
 	if self.mode == "vertical" then
 		love.graphics.rectangle("fill", self.x, self.y, self.w, self.h*progress)
 	else
 		love.graphics.rectangle("fill", self.x, self.y, self.w*progress, self.h)
 	end
+	love.graphics.setColor(1, 1, 1, 1)
 end
 
 function drawEndScreen()
